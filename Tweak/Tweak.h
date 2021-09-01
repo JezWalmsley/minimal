94846a9c6195ee44369221a14dc1bd5a2e8d0e8c
RuntimeOverflow <mail@runtimeoverflow.com>
2021-09-01 23:00:09 +0200

    Tap to show banner

--- Tweak/Tweak.h
+++ Tweak/Tweak.h
@@ -2,8 +2,6 @@
 #import <dlfcn.h>
 #import <SpringBoard/SpringBoard.h>
 
-BOOL isBanner = FALSE;
-
 @interface SBBannerManager : NSObject
 @property UIWindow *bannerWindow;
 @end
@@ -27,11 +25,34 @@ BOOL isBanner = FALSE;
 @property (nonatomic, strong, readwrite) UILabel *dateView;
 @end
 
+@interface BBAction : NSObject
++ (id)actionWithLaunchBundleID:(id)arg1 callblock:(id)arg2;
+@end
+
+@interface BBBulletin : NSObject
+@property(nonatomic, copy)NSString* sectionID;
+@property(nonatomic, copy)NSString* header;
+@property(nonatomic, copy)NSString* section; 
+@property(nonatomic, copy)NSString* recordID;
+@property(nonatomic, copy)NSString* publisherBulletinID;
+@property(nonatomic, copy)NSString* title;
+@property(nonatomic, copy)NSString* message;
+@property(nonatomic, retain)NSDate* date;
+@property(assign, nonatomic)BOOL clearable;
+@property(nonatomic)BOOL showsMessagePreview;
+@property(nonatomic, copy)BBAction* defaultAction;
+@property(nonatomic, copy)NSString* bulletinID;
+@property(nonatomic, retain)NSDate* lastInterruptDate;
+@property(nonatomic, retain)NSDate* publicationDate;
+@property(nonatomic, copy)NSString* threadID;
+@end
+
 @interface NCNotificationContent : NSObject
 @property (nonatomic, assign, readonly) UIImage *icon;
 @end
 
 @interface NCNotificationRequest : NSObject
+@property (nonatomic,readonly) BBBulletin *bulletin;
 @property (nonatomic, assign, readonly) NCNotificationContent *content;
 @end
 
@@ -69,28 +90,10 @@ BOOL isBanner = FALSE;
 @property (nonatomic, strong, readwrite) SBNotificationPresentableViewController *presentable;
 @end
 
-@interface BBAction : NSObject
-+ (id)actionWithLaunchBundleID:(id)arg1 callblock:(id)arg2;
-@end
-
-@interface BBBulletin : NSObject
-@property(nonatomic, copy)NSString* sectionID;
-@property(nonatomic, copy)NSString* recordID;
-@property(nonatomic, copy)NSString* publisherBulletinID;
-@property(nonatomic, copy)NSString* title;
-@property(nonatomic, copy)NSString* message;
-@property(nonatomic, retain)NSDate* date;
-@property(assign, nonatomic)BOOL clearable;
-@property(nonatomic)BOOL showsMessagePreview;
-@property(nonatomic, copy)BBAction* defaultAction;
-@property(nonatomic, copy)NSString* bulletinID;
-@property(nonatomic, retain)NSDate* lastInterruptDate;
-@property(nonatomic, retain)NSDate* publicationDate;
-@end
-
 @interface BBServer : NSObject
 - (void)publishBulletin:(BBBulletin *)arg1 destinations:(NSUInteger)arg2 alwaysToLockScreen:(BOOL)arg3;
 - (void)publishBulletin:(id)arg1 destinations:(unsigned long long)arg2;
+- (void)_clearBulletinIDs:(id)arg1 forSectionID:(id)arg2 shouldSync:(BOOL)arg3;
 @end
 
 @interface BBObserver : NSObject
